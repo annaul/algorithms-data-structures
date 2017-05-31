3508a59d0a7f2de60969690071dd94a0d486ed24
Anna Ulanova <anetteua0@gmail.com>
2017-05-30 23:49:38 -0700

    max revenue fixed

--- greedy/ad_revenue.js
+++ greedy/ad_revenue.js
@@ -13,13 +13,21 @@ var numAds;
 var profitPerClick = [];
 var clicksPerDay = [];
 
-function maxRevenue(numAds, profitPerClick, clicksPerDay) {
-  profitPerClick = profitPerClick.sort().reverse();
-  clicksPerDay = clicksPerDay.sort().reverse();
-  var value = 0;
+function orderReverse (array) {
+  array.sort(function(a, b) {
+  return b - a;
+});
+}
 
+function maxRevenue(numAds, profitPerClick, clicksPerDay) {
+  orderReverse(profitPerClick);
+  orderReverse(clicksPerDay);
+  var value = 0;
   for (var i = 0; i < numAds; i++) {
-    value += profitPerClick[i] * clicksPerDay[i];
+    if (profitPerClick.length === 0 || clicksPerDay.length === 0) break;
+    value += profitPerClick[0] * clicksPerDay[0];
+    profitPerClick.shift();
+    clicksPerDay.shift();
   }
   return value;
 }
