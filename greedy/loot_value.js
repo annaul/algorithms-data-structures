904b6b0a1d91942b9d5887c91cb05f5caf6b1ae1
Anna Ulanova <anetteua0@gmail.com>
2017-05-30 19:42:13 -0700

    manual insert sorting works

---
+++ greedy/loot_value.js
@@ -0,0 +1,60 @@
+var readline = require('readline');
+
+process.stdin.setEncoding('utf8');
+var rl = readline.createInterface({
+  input: process.stdin,
+  terminal: false
+});
+
+rl.on('line', readLine);
+
+var lineNum = 0;
+
+var numItems;
+var knapsackCapacity;
+var items = [];
+var sortedItems = [];
+
+function solution(numItems, knapsackCapacity, items) {
+
+}
+
+function sortArray(items) {
+  for (var i = 0; i < items.length; i++) {
+    if (i === 0) sortedItems.push(items[i]);
+    else if (items[i][0]/items[i][1] >= sortedItems[0][0]/sortedItems[0][1]) sortedItems.unshift(items[i]);
+    else if (items[i][0]/items[i][1] <= sortedItems[i-1][0]/sortedItems[i-1][1]) sortedItems.push(items[i]);
+    else {
+      var a = i-1;
+      while(items[i][1]/items[i][0] < sortedItems[a][1]/sortedItems[a][0]) {
+        a -= 1;
+      }
+      sortedItems.splice(a+1, 0, items[i]);
+    }
+  }
+  return sortedItems;
+}
+
+sortArray(items);
+
+function readLine (line) {
+  line = line.trim();
+  if (line == '') {
+    return;
+  }
+
+  var parts = line.toString().split(' ').map(function (x) { return parseInt(x, 10) });
+
+  if (lineNum == 0) {
+    numItems = parts[0];
+    knapsackCapacity = parts[1];
+  } else {
+    items.push(parts)
+    if (items.length == numItems) {
+      console.log(numItems, knapsackCapacity, sortArray(items));
+      process.exit();
+    }
+  }
+
+  lineNum++;
+}
