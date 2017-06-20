d1bfa50ee95d64efdd90c171549aa3cfeaee529f
Anna Ulanova <anetteua0@gmail.com>
2017-06-20 11:16:58 -0700

    changed sort and majority counting

--- divide_and_conquer/majority_search.js
+++ divide_and_conquer/majority_search.js
@@ -13,37 +13,53 @@ var num = 0;
 var numArr = [];
 var majority = numArr[0];
 var counter = 1;
-var newArr = [];
+var median = null;
 
 if (num !== numArr.length) return 'wrong input';
 
+// function sortArr(numArr) {
+//   for (var i = 0; i < numArr.length; i++) {
+//     if (i === 0) {
+//       newArr.push(numArr[i]);
+//     } else if (numArr[i] <= newArr[0]) {
+//       newArr.unshift(numArr[i]);
+//     } else if (numArr[i] >= newArr[i-1]) {
+//       newArr.push(numArr[i]);
+//     } else {
+//       var a = i-1;
+//       while(newArr[a] > numArr[i]) {
+//         a -= 1;
+//       }
+//       newArr.splice(a+1, 0, numArr[i]);
+//     }
+//   }
+//   return numArr;
+// }
+
 function sortArr(numArr) {
-  for (var i = 0; i < numArr.length; i++) {
-    if (i === 0) {
-      newArr.push(numArr[i]);
-    } else if (numArr[i] <= newArr[0]) {
-      newArr.unshift(numArr[i]);
-    } else if (numArr[i] >= newArr[i-1]) {
-      newArr.push(numArr[i]);
-    } else {
-      var a = i-1;
-      while(newArr[a] > numArr[i]) {
-        a -= 1;
-      }
-      newArr.splice(a+1, 0, numArr[i]);
-    }
-  }
-  return newArr;
+  numArr.sort(function (a, b) {
+    return a - b;
+  });
+  return numArr;
+}
+
+function findMedian(numArr) {
+  var middle = numArr.length % 2;
+  var medianInd = Math.ceil(numArr.length / 2) - 1;
+  median = numArr[medianInd];
+  return median;
 }
 
 function findMajority(numArr) {
   sortArr(numArr);
-  var middle = newArr.length % 2;
-  var medianInd = Math.ceil(newArr.length / 2) - 1;
-  var median = newArr[medianInd];
-  if (middle === 1 && newArr[0] === median) return 1;
-  if (middle === 0 && newArr[0] === median && newArr[medianInd + 1] === median) return 1;
-  return 0;
+  findMedian(numArr);
+  var counter = 0;
+
+  for (var i = 0; i < numArr.length; i++) {
+    if (numArr[i] = median) counter += 1;
+  }
+  if ((numArr.length / 2 + 1) > counter) return 0;
+  else return 1;
 }
 
 function readLine (line) {
