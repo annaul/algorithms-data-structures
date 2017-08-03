8a6e77abbd200687ac36301a3df20efe7df1be08
Anna Ulanova <anetteua0@gmail.com>
2017-08-03 16:14:34 -0700

    saving

--- data_structures_class/priority_queues_and_disjoint_sets/make_heap.js
+++ data_structures_class/priority_queues_and_disjoint_sets/make_heap.js
@@ -9,29 +9,38 @@ var rl = readline.createInterface({
 rl.on('line', readLine);
 var lineNum = 0;
 var num;
+var arr = [];
 var swaps = [];
 
-function siftUp(max, min) {
-  var hold = min;
-  min = max;
-  max = hold;
-}
-
 function makeHeap(arr) {
-  for (var i = 0; i <= arr.length; i++) {
-
+  for (var i = arr.length - 1; i >= 0; --i) {
     if (2 * i + 1 <= arr.length) {
       if (arr[i] > arr[2 * i + 1]) {
-        siftUp(arr[i], arr[2 * i + 1]);
-        swaps.push(arr[i], arr[2 * i + 1]);
+        var temp1 = arr[i];
+        arr[i] = arr[2 * i + 1];
+        arr[2 * i + 1] = temp1;
+
+        swaps.push(i, 2 * i + 1);
       }
     }
     if (2 * i + 2 <= arr.length) {
+      if (arr[i] > arr[2 * i + 2]) {
+        var temp2 = arr[i];
+        arr[i] = arr[2 * i + 2];
+        arr[2 * i + 2] = temp2;
 
+        swaps.push(i, 2 * i + 2);
+      }
     }
   }
+  return swaps;
 }
 
+function displayResult(arr) {
+  for (var i = 0; i <= arr.length/2; i += 2) {
+    console.log(arr[i], arr[i+1]);
+  }
+}
 
 function readLine (line) {
   line = line.trim();
@@ -44,9 +53,13 @@ function readLine (line) {
   if (lineNum === 0) {
     num = parts[0];
   } else if (lineNum === 1){
-    var arr = parts;
-    console.log(num, arr);
+    arr = parts;
 
+    makeHeap(arr);
+    console.log(swaps.length / 2);
+    if (swaps.length > 0) {
+      displayResult(makeHeap(arr));
+    }
     process.exit();
   }
   lineNum++;
